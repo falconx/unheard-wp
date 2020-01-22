f9103fd3ad927b4d614df78b7140bdfeb7d769c5
Matt Layton <mattlayt@gmail.com>
2020-01-22 22:08:30 +0000

    fix: update deploy plugin to be toolbar link

--- wp-content/plugins/unheard-deploy/uneard-deploy.php
+++ wp-content/plugins/unheard-deploy/uneard-deploy.php
@@ -3,33 +3,15 @@
 Plugin Name: Deploy
 */
 
-/**
- * Trigger CI build
- */
-function trigger_build() {
-  $url = 'https://api.netlify.com/build_hooks/5e28b8b6734d8f1bea66fc90';
-
-  // request headers
-  $headers = array(
-    'Content-Type: application/x-www-form-urlencoded',
-  );
-
-  // open connection
-  $ch = curl_init();
-
-  // set the url, number of POST vars, POST data
-  curl_setopt($ch, CURLOPT_VERBOSE, 1);
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, "{}");
-
-  // execute post
-  curl_exec($ch);
-
-  // close connection
-  curl_close($ch);
+function add_toolbar_items($admin_bar) {
+  $admin_bar->add_node(array(
+    'id' => 'deploy',
+    'title' => 'Deploy',
+    'href' => '#',
+    'meta' => array(
+      'onclick' => '(function() { const xhr = new XMLHttpRequest(); const url = "https://api.netlify.com/build_hooks/5e28b8b6734d8f1bea66fc90"; xhr.open("POST", url, true); xhr.send(); alert("Deploying"); })()',
+    ),
+  ));
 }
 
-add_action('update_post', 'trigger_build');
+add_action('admin_bar_menu', 'add_toolbar_items', 100);
